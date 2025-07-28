46ba9a16c08071d80292b27ea060bc7f2972583b
Yamini Ahuja <yaminiahuja1703@gmail.com>
2025-07-28 22:17:17 +0530

    Update server.js

--- backend/src/server.js
+++ backend/src/server.js
@@ -21,7 +21,14 @@ const app = express();
 
 
 app.use(express.json());
+
 app.use(cors({ //cjeck and learn about them
+  origin: 'http://localhost:5173',
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  credentials: true  
+}));
+
+app.options("*",cors({ 
   origin: 'http://localhost:5173',
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   credentials: true  
